7e7b2189c631bc4ee3248998d0b7ac87b947ad93
Emeka Orji <emekapraiseo@gmail.com>
2021-11-18 07:15:46 +0100

    Created a new Branch

--- assets/js/layout.js
+++
@@ -1,6 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-var navBar = document.querySelector(".navbar");
-hamburger.addEventListener("click", function () {
-  this.parentNode.parentNode.classList.toggle("navbar_active");
-  navBar.classList.toggle("navbar_active");
-});
